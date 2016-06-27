d70217ffb6b09f993e47e74cbf7ff223bf94f737
samwu <sam.wu@embest-tech.com>
2016-06-26 18:10:28 -0700

    BUG:Fix the bug "missing expression between '(' ')'" for net/net.c
    Signed-off-by: samwu <sam.wu@embest-tech.com>

--- net/net.c
+++ net/net.c
@@ -420,7 +420,7 @@ restart:
 			CDPStart();
 			break;
 #endif
-#if defined (CONFIG_NETCONSOLE) && !(CONFIG_SPL_BUILD)
+#if defined (CONFIG_NETCONSOLE) && !defined (CONFIG_SPL_BUILD)
 		case NETCONS:
 			NcStart();
 			break;
@@ -1183,7 +1183,7 @@ NetReceive(uchar *inpkt, int len)
 #endif
 
 
-#if defined (CONFIG_NETCONSOLE) && !(CONFIG_SPL_BUILD)
+#if defined (CONFIG_NETCONSOLE) && !defined(CONFIG_SPL_BUILD)
 		nc_input_packet((uchar *)ip + IP_UDP_HDR_SIZE,
 					src_ip,
 					ntohs(ip->udp_dst),
